339475b06353226c07e2a66df89eb966c185e970
Samiran <samiran.kakoty@gmail.com>
2020-01-12 17:21:29 +0530

    Fetching Content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
   constructor() {
     super();
-
     this.state = {
-      string: 'Hello Samiran'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'ada1'
+        },
+        {
+          name: 'Dracula',
+          id: 'ads3'
+        },
+        {
+          name: 'Zombie',
+          id: 'adm1'
+        }
+      ]
     };
   }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
+  }
+
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Welcome to React' })}>Change Text</button>
-        </header>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}> { monster.name } </h1>
+        ))}
       </div>
     );
   }
